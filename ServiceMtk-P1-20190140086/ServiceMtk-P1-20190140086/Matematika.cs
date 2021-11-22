5f4b2da4e128b04f78b65601db81ef6bffec1c79
Abdil Tegar Arifin <abdiltegar12@gmail.com>
2021-11-22 19:38:33 +0700

    Modul 6

---
+++ ServiceMtk-P1-20190140086/ServiceMtk-P1-20190140086/Matematika.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace ServiceMtk_P1_20190140086
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in both code and config file together.
+    public class Matematika : IMatematika
+    {
+        public int Bagi(int a, int b)
+        {
+            try {
+                return a / b;
+            }catch(Exception e){
+                Console.WriteLine(e.Message);
+
+                MathFault mf = new MathFault();
+                mf.Kode = "Inputan yang anda masukkan salah";
+                mf.Pesan = "Masukkan inputan yang benar";
+                throw new FaultException<MathFault>(mf);
+            }
+        }
+        public int Kali(int a, int b)
+        {
+            try { 
+                return a * b;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+
+                MathFault mf = new MathFault();
+                mf.Kode = "Inputan yang anda masukkan salah";
+                mf.Pesan = "Masukkan inputan yang benar";
+                throw new FaultException<MathFault>(mf);
+            }
+        }
+        public int Kurang(int a, int b)
+        {
+            try
+            {
+                return a - b;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+
+                MathFault mf = new MathFault();
+                mf.Kode = "Inputan yang anda masukkan salah";
+                mf.Pesan = "Masukkan inputan yang benar";
+                throw new FaultException<MathFault>(mf);
+            }
+        }
+        public int Tambah(int a, int b)
+        {
+            try {
+                return a + b;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+
+                MathFault mf = new MathFault();
+                mf.Kode = "Inputan yang anda masukkan salah";
+                mf.Pesan = "Masukkan inputan yang benar";
+                throw new FaultException<MathFault>(mf);
+            }
+        }
+        //TKoordinat:nama methodnya, a,b = input seperti int.
+        //Membuat koordinat hasil lalu menjumlahkan xa dengan xb, ya dengan yb lalu dimasukkan ke hasil
+        public Koordinat TKoordinat(Koordinat a, Koordinat b)
+        {
+            try { 
+                Koordinat hasil = new Koordinat();
+                hasil.X = a.X + b.X;
+                hasil.Y = a.Y + b.Y;
+                return hasil;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+
+                MathFault mf = new MathFault();
+                mf.Kode = "Inputan yang anda masukkan salah";
+                mf.Pesan = "Masukkan inputan yang benar";
+                throw new FaultException<MathFault>(mf);
+            }
+        }
+    }
+}
